c1591d6d2c15c10ef850ec97594192a175ef6b84
suyanlong <592938410@qq.com>
2018-02-09 16:42:49 +0800

    fixed bug and update README.md

--- README.md
+++ README.md
@@ -1,40 +1,60 @@
 # dbcompare
 level db, bolt db, badger db and bunt db benchmark compare
 ```go
-go test -bench .
+cd benchmark
 
-*** b2d5aad commit benchmark result****
+first run: go test -bench .
+
 goos: linux
 goarch: amd64
 pkg: github.com/suyanlong/dbcompare/benchmark
-BenchmarkLevelDbPut-4             200000             12916 ns/op
-BenchmarkBoltUpdate-4                500           2663169 ns/op
-BenchmarkBadgerUpdate-4             1000           2531718 ns/op
-BenchmarkGoLevelDbBindPut-4       300000             30954 ns/op
-BenchmarkBuntDbPut-4              200000             12824 ns/op
-BenchmarkLevelDbGet-4             300000              7776 ns/op
-BenchmarkBoltUpdateGet-4            1000           2048873 ns/op
-BenchmarkBadgerUpdateGet-4      *** Test killed: ran too long (10m0s).  ==> fuck!
-FAIL    github.com/suyanlong/dbcompare/benchmark        605.038s
-
-
-
-*** current commit benchmark result****
-goos: linux
-goarch: amd64
-pkg: github.com/suyanlong/dbcompare/benchmark
-BenchmarkLevelDbPut-4              10000            326518 ns/op
-BenchmarkBoltUpdate-4              20000             84175 ns/op
-BenchmarkBadgerUpdate-4            50000             27792 ns/op
-BenchmarkGoLevelDbBindPut-4        10000            113293 ns/op
-BenchmarkBuntDbPut-4              100000             31235 ns/op
-BenchmarkLevelDbGet-4             100000             83952 ns/op
-BenchmarkBoltUpdateGet-4          100000            137974 ns/op
-BenchmarkBadgerUpdateGet-4       1000000              2903 ns/op
-BenchmarkGoLevelDbBindGet-4       100000            119436 ns/op
-BenchmarkBuntDbGet-4              500000              5171 ns/op
+BenchmarkLevelDbSet-4                 20         109431183 ns/op
+BenchmarkBoltSet-4                    10         172565745 ns/op
+BenchmarkBadgerSet-4                  30          73219897 ns/op
+BenchmarkGoLevelDbSet-4               30         100224908 ns/op
+BenchmarkBuntDbSet-4                  30          37309376 ns/op
+BenchmarkLevelDbGet-4              50000             21898 ns/op
+BenchmarkBoltGet-4                   500           2474387 ns/op
+BenchmarkBadgerGet-4              500000              2981 ns/op
+BenchmarkGoLevelDbGet-4           300000              5632 ns/op
+BenchmarkBuntDbGet-4             1000000              2944 ns/op
 PASS
-ok      github.com/suyanlong/dbcompare/benchmark        353.750s
+ok      github.com/suyanlong/dbcompare/benchmark        35.536s
+
+two run: go test -bench . -v
+goos: linux
+goarch: amd64
+pkg: github.com/suyanlong/dbcompare/benchmark
+BenchmarkLevelDbSet-4                 20         374127785 ns/op
+BenchmarkBoltSet-4                    10         140051018 ns/op
+BenchmarkBadgerSet-4                  20          68027663 ns/op
+BenchmarkGoLevelDbSet-4               30         170119310 ns/op
+BenchmarkBuntDbSet-4                  30          66782111 ns/op
+BenchmarkLevelDbGet-4             100000             22287 ns/op
+BenchmarkBoltGet-4                   500           2496795 ns/op
+BenchmarkBadgerGet-4              500000              2282 ns/op
+BenchmarkGoLevelDbGet-4           300000              5323 ns/op
+BenchmarkBuntDbGet-4              500000              3649 ns/op
+PASS
+ok      github.com/suyanlong/dbcompare/benchmark        52.046s
+
+three run: go test -bench . -v
+
+goos: linux
+goarch: amd64
+pkg: github.com/suyanlong/dbcompare/benchmark
+BenchmarkLevelDbSet-4                 20         607825638 ns/op
+BenchmarkBoltSet-4                     5         205219111 ns/op
+BenchmarkBadgerSet-4                  30          72856209 ns/op
+BenchmarkGoLevelDbSet-4               30         300955982 ns/op
+BenchmarkBuntDbSet-4                  20          50155096 ns/op
+BenchmarkLevelDbGet-4             100000             20660 ns/op
+BenchmarkBoltGet-4                   500           2834335 ns/op
+BenchmarkBadgerGet-4             1000000              2350 ns/op
+BenchmarkGoLevelDbGet-4           300000              5599 ns/op
+BenchmarkBuntDbGet-4              500000              3697 ns/op
+PASS
+ok      github.com/suyanlong/dbcompare/benchmark        69.769s
 
 
 ```
--- benchmark/benchmark_test.go
+++ benchmark/benchmark_test.go
@@ -1,171 +1,198 @@
 package benchmark
 
 import (
+	"bytes"
 	"github.com/boltdb/bolt"
 	"github.com/dgraph-io/badger"
 	goleveldb "github.com/golang/leveldb"
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/tidwall/buntdb"
+	"math/rand"
 	"strconv"
 	"testing"
 )
 
-var value = RandStringBytesMaskImprSrc(5120)
+var value = RandStringBytesMaskImprSrc(512)
 var valueByte = []byte(value)
 
-func BenchmarkLevelDbPut(b *testing.B) {
-	db, _ := leveldb.OpenFile(" /tmp/level.db", nil)
+func BenchmarkLevelDbSet(b *testing.B) {
+	db, _ := leveldb.OpenFile("level.db", nil)
 	defer db.Close()
+
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		db.Put([]byte(strconv.Itoa(i)), valueByte, nil)
+		batch := new(leveldb.Batch)
+		for i := 0; i < 10000; i++ {
+			batch.Put([]byte(strconv.Itoa(int(rand.Int63()))), valueByte)
+		}
+		//b.SetBytes(int64(cap(batch)))
+		db.Write(batch, nil)
 	}
 	b.StopTimer()
 }
 
-func BenchmarkBoltUpdate(b *testing.B) {
-	db := MustOpenDB()
-	defer db.MustClose()
-	if err := db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucket([]byte("bench"))
-		return err
-	}); err != nil {
-		b.Fatal(err)
-	}
+func BenchmarkBoltSet(b *testing.B) {
+	db, _ := bolt.Open("bolt.db", 0666, nil)
+	defer db.Close()
 
 	b.ResetTimer()
-	db.Update(func(tx *bolt.Tx) error {
-		bk := tx.Bucket([]byte("bench"))
-		for i := 0; i < b.N; i++ {
-			bk.Put([]byte(strconv.Itoa(i)), valueByte)
-		}
-		return nil
-	})
+	for i := 0; i < b.N; i++ {
+		db.Update(func(tx *bolt.Tx) error {
+			bk, _ := tx.CreateBucketIfNotExists([]byte(strconv.Itoa(i)))
+			for i := 0; i < 10000; i++ {
+				bk.Put([]byte(strconv.Itoa(int(rand.Int63()))), valueByte)
+			}
+			return nil
+		})
+	}
 	b.StopTimer()
 }
 
-func BenchmarkBadgerUpdate(b *testing.B) {
+func BenchmarkBadgerSet(b *testing.B) {
 	opts := badger.DefaultOptions
-	opts.Dir = "/tmp/badger"
-	opts.ValueDir = "/tmp/badger"
+	opts.Dir = "badger"
+	opts.ValueDir = "badger"
 	db, _ := badger.Open(opts)
 	defer db.Close()
-	// Your code here…
-	b.ResetTimer()
-	db.Update(func(txn *badger.Txn) error {
-		for i := 0; i < b.N; i++ {
-			txn.Set([]byte(strconv.Itoa(i)), valueByte)
-		}
-		return nil
-	})
-	b.StopTimer()
-}
 
-func BenchmarkGoLevelDbBindPut(b *testing.B) {
-	db, _ := goleveldb.Open("/tmp/goleveldb", nil)
-	defer db.Close()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		db.Set([]byte(strconv.Itoa(i)), valueByte, nil)
+		db.Update(func(txn *badger.Txn) error {
+			for i := 0; i < 10000; i++ {
+				txn.Set([]byte(strconv.Itoa(int(rand.Int63()))), valueByte)
+			}
+			return nil
+		})
 	}
 	b.StopTimer()
 }
 
-func BenchmarkBuntDbPut(b *testing.B) {
+func BenchmarkGoLevelDbSet(b *testing.B) {
+	db, _ := goleveldb.Open("goleveldb", nil)
+	defer db.Close()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		batch := goleveldb.Batch{}
+		for i := 0; i < 10000; i++ {
+			batch.Set([]byte(strconv.Itoa(int(rand.Int63()))), valueByte)
+		}
+		db.Apply(batch, nil)
+	}
+	b.StopTimer()
+}
+
+func BenchmarkBuntDbSet(b *testing.B) {
 	db, _ := buntdb.Open("bunt.db")
 	defer db.Close()
 
 	b.ResetTimer()
-	db.Update(func(tx *buntdb.Tx) error {
-		for i := 0; i < b.N; i++ {
-			tx.Set(strconv.Itoa(i), value, nil)
-		}
-		return nil
-	})
+	for i := 0; i < b.N; i++ {
+		db.Update(func(tx *buntdb.Tx) error {
+			for i := 0; i < 10000; i++ {
+				tx.Set(strconv.Itoa(int(rand.Int63())), value, nil)
+			}
+			return nil
+		})
+	}
 	b.StopTimer()
 }
 
 // Benchmark read data
 func BenchmarkLevelDbGet(b *testing.B) {
-	db, _ := leveldb.OpenFile(" /tmp/level.db", nil)
+	db, _ := leveldb.OpenFile("level.db", nil)
 	defer db.Close()
 
-	for i := 0; i < b.N; i++ {
-		db.Put([]byte(strconv.Itoa(i)), valueByte, nil)
+	batch := new(leveldb.Batch)
+	for i := 0; i < 10000; i++ {
+		batch.Put([]byte(strconv.Itoa(i)), valueByte)
 	}
+	db.Write(batch, nil)
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		db.Get([]byte(strconv.Itoa(i)), nil)
+		val, _ := db.Get([]byte(strconv.Itoa(rand.Intn(10000))), nil)
+		if !bytes.Equal(val, valueByte) {
+			panic("BenchmarkLevelDbGet error")
+		}
 	}
-
 	b.StopTimer()
 }
 
-func BenchmarkBoltUpdateGet(b *testing.B) {
-	db := MustOpenDB()
-	defer db.MustClose()
-	if err := db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucket([]byte("bench"))
-		return err
-	}); err != nil {
-		b.Fatal(err)
-	}
-
+func BenchmarkBoltGet(b *testing.B) {
+	db, _ := bolt.Open("bolt.db", 0666, nil)
+	defer db.Close()
 	db.Update(func(tx *bolt.Tx) error {
-		bk := tx.Bucket([]byte("bench"))
-		for i := 0; i < b.N; i++ {
+		bk, _ := tx.CreateBucketIfNotExists([]byte("bench"))
+		for i := 0; i < 10000; i++ {
 			bk.Put([]byte(strconv.Itoa(i)), valueByte)
 		}
 		return nil
 	})
 
 	b.ResetTimer()
-	db.Update(func(tx *bolt.Tx) error {
-		bk := tx.Bucket([]byte("bench"))
-		for i := 0; i < b.N; i++ {
-			bk.Get([]byte(strconv.Itoa(i)))
-		}
-		return nil
-	})
+	for i := 0; i < b.N; i++ {
+		db.Update(func(tx *bolt.Tx) error {
+			bk := tx.Bucket([]byte("bench"))
+			if !bytes.Equal(valueByte, bk.Get([]byte(strconv.Itoa(rand.Intn(10000))))) {
+				panic("BenchmarkBoltGet error")
+			}
+			return nil
+		})
+	}
 	b.StopTimer()
 }
 
-func BenchmarkBadgerUpdateGet(b *testing.B) {
+func BenchmarkBadgerGet(b *testing.B) {
 	opts := badger.DefaultOptions
-	opts.Dir = "/tmp/badger"
-	opts.ValueDir = "/tmp/badger"
+	opts.Dir = "badger"
+	opts.ValueDir = "badger"
 	db, _ := badger.Open(opts)
 	defer db.Close()
-	// Your code here…
+
 	db.Update(func(txn *badger.Txn) error {
-		for i := 0; i < b.N; i++ {
+		for i := 0; i < 10000; i++ {
 			txn.Set([]byte(strconv.Itoa(i)), valueByte)
 		}
 		return nil
 	})
 
 	b.ResetTimer()
-	db.Update(func(txn *badger.Txn) error {
-		for i := 0; i < b.N; i++ {
-			txn.Get([]byte(strconv.Itoa(i)))
-		}
-		return nil
-	})
+	for i := 0; i < b.N; i++ {
+		db.View(func(txn *badger.Txn) error {
+			item, _err := txn.Get([]byte(strconv.Itoa(rand.Intn(10000))))
+			if _err != nil {
+				panic("BenchmarkBadgerGet error")
+			} else {
+				if val, err := item.Value(); err == nil {
+					if !bytes.Equal(valueByte, val) {
+						panic("BenchmarkBadgerGet error")
+					}
+				} else {
+					panic("BenchmarkBadgerGet error")
+				}
+			}
+			return nil
+		})
+	}
 	b.StopTimer()
 }
 
-func BenchmarkGoLevelDbBindGet(b *testing.B) {
-	db, _ := goleveldb.Open("/tmp/goleveldb", nil)
+func BenchmarkGoLevelDbGet(b *testing.B) {
+	db, _ := goleveldb.Open("goleveldb", nil)
 	defer db.Close()
 
-	for i := 0; i < b.N; i++ {
-		db.Set([]byte(strconv.Itoa(i)), valueByte, nil)
+	batch := goleveldb.Batch{}
+	for i := 0; i < 10000; i++ {
+		batch.Set([]byte(strconv.Itoa(i)), valueByte)
 	}
+	db.Apply(batch, nil)
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		db.Get([]byte(strconv.Itoa(i)), nil)
+		val, _ := db.Get([]byte(strconv.Itoa(rand.Intn(10000))), nil)
+		if !bytes.Equal(val, valueByte) {
+			panic("BenchmarkGoLevelDbGet error")
+		}
 	}
 	b.StopTimer()
 }
@@ -175,18 +202,21 @@ func BenchmarkBuntDbGet(b *testing.B) {
 	defer db.Close()
 
 	db.Update(func(tx *buntdb.Tx) error {
-		for i := 0; i < b.N; i++ {
+		for i := 0; i < 10000; i++ {
 			tx.Set(strconv.Itoa(i), value, nil)
 		}
 		return nil
 	})
 
 	b.ResetTimer()
-	db.View(func(tx *buntdb.Tx) error {
-		for i := 0; i < b.N; i++ {
-			tx.Get(strconv.Itoa(i))
-		}
-		return nil
-	})
+	for i := 0; i < b.N; i++ {
+		db.View(func(tx *buntdb.Tx) error {
+			val, _ := tx.Get(strconv.Itoa(rand.Intn(10000)))
+			if val != value {
+				panic("BenchmarkBuntDbGet error")
+			}
+			return nil
+		})
+	}
 	b.StopTimer()
 }
